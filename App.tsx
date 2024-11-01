38fe99c642cc609863c63475b647d1146c55feba
techwithtwin <realwebtechguru@gmail.com>
2024-11-01 22:51:53 +0300

    adding a delete button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Delete", "Are you sure you want to delete this item?", [
+      {
+        text: "No",
+        onPress: () => console.log("No"),
+      },
+      {
+        text: "Yes",
+        onPress: () => console.log("Yes"),
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
-      {true && <Text>Hey</Text>}
-      <Text>Hello let's start doing this</Text>
+      <View style={styles.innerBox}>
+        <Text style={styles.text}>Coffee</Text>
+        <TouchableOpacity style={styles.button} onPress={handleDelete}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,8 +29,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+
+    backgroundColor: theme.colorWhite,
     justifyContent: "center",
+  },
+  innerBox: {
+    borderBottomColor: theme.colorCerulean,
+    borderBottomWidth: 1,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "400",
+  },
+  button: {
+    backgroundColor: "red",
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: theme.colorWhite,
+    textTransform: "uppercase",
   },
 });
